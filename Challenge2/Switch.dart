51a55b9e26cb5d4f88ec448d7e6a401db55a4ebc
Dhinakaran K <dhinadts@gmail.com>
2025-07-07 16:44:41 +0530

    Challange 2 - Dart Switch

---
+++ Challenge2/Switch.dart
@@ -0,0 +1,15 @@
+void main() {
+  int day = 7;
+
+  if (day <= 5) {
+    switch (day) {
+      default:
+        print('Weekday');
+    }
+  } else {
+    switch (day) {
+      default:
+        print('Weekend');
+    }
+  }
+}
